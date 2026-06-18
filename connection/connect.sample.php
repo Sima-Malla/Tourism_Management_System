717e4b76657a6cf06c18ab5be3fd511abd319012
Sima-Malla <sima.malla2004@gmail.com>
2026-06-18 15:15:51 +0545

    Remove sensitive files from tracking and add .gitignore

--- connection/connect.sample.php
+++ connection/connect.sample.php
@@ -1,17 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "hotel";
+$username   = "your_db_username";
+$password   = "your_db_password";
+$dbname     = "your_db_name";
 
-
-// Create connection
 $db = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
 if (!$db) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
-
 ?>
